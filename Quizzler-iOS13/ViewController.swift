aa14a0183b1dec09bcc4621580d946d240aa2706
m00racle <yanuar.prakosa@gmail.com>
2021-07-27 08:29:55 +0700

    connect outlet and actions to viewController

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionText: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBAction func trueButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func falseButtonPressed(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
